bc92658d7e73bab3b63a6d26004e975cc5c0b187
Guilherme Luiz <guilhermelc10@gmail.com>
2024-09-27 21:45:35 -0300

    Questão String

--- Projeto.py
+++ Projeto.py
@@ -13,15 +13,30 @@ def Fibonacci():
 
 
         if fib == num:
-            return "Está na sequência"
+            return "Está na sequência\n"
 
         else:
-            return "Não está na sequência"
+            return "Não está na sequência\n"
         
     except ValueError:
-        print("Entrada Inválida")
+        return "Entrada Inválida\n"
     
+def verificar_palavra():
+    palavra = input('Digite uma palavra:')
 
+    if 'a' in palavra.lower():
+        quantidade = palavra.count('a')
+        return f"\nA letra a existe na palavra {palavra}\nQuantidade de vezes que a letra a se repete: {palavra.count('a')}"
+       
+    else:
+        return f"\nA letra a não aparece na palavra {palavra}"
+    
+    
 print("Questão 1 - Fibonacci")
 print(Fibonacci())
+
+print("Questão 2 - Teste String")
+print(verificar_palavra())
+
+
 
